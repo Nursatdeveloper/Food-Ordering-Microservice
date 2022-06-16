abac32dfd58290353bdca387c4b82bbfb4928c53
Nursat Zeinolla <kznursat@gmail.com>
2022-06-16 20:25:36 +0600

    Created Api Gateway

---
+++ Api_Gateway/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration["SecretKey"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false
+
+    };
+});
+
+builder.Configuration.AddJsonFile(Path.Combine("Configuration", "configuration.Development.json"));
+
+builder.Services.AddOcelot();
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseAuthentication();
+
+app.UseOcelot();
+
+app.Run();
